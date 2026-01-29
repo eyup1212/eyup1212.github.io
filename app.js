3fbc52900f52d614af5d6b228bfb8038a9b6e2ba
eyup1212 <dream1212dreamte@gmail.com>
2026-01-29 19:08:40 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,60 @@
+// USER
+function login() {
+  let name = document.getElementById("name").value;
+  if (!name) return;
+
+  let id = Math.random().toString(36).slice(2);
+  localStorage.setItem("uid", id);
+  localStorage.setItem("messages", "[]");
+
+  document.getElementById("login").style.display = "none";
+  document.getElementById("app").style.display = "block";
+
+  document.getElementById("link").value =
+    location.origin + "/send.html?to=" + id;
+
+  loadMessages();
+}
+
+// LOAD MESSAGES
+function loadMessages() {
+  let msgs = JSON.parse(localStorage.getItem("messages") || "[]");
+  let ul = document.getElementById("messages");
+  ul.innerHTML = "";
+  msgs.reverse().forEach(m => {
+    let li = document.createElement("li");
+    li.textContent = m;
+    ul.appendChild(li);
+  });
+}
+
+// SEND MESSAGE
+function send() {
+  let params = new URLSearchParams(location.search);
+  let to = params.get("to");
+  let text = document.getElementById("msg").value;
+  if (!text) return;
+
+  // SAHTE RAM SİMÜLASYONU
+  let key = "inbox_" + to;
+  let box = JSON.parse(localStorage.getItem(key) || "[]");
+  box.push(text);
+  localStorage.setItem(key, JSON.stringify(box));
+
+  alert("Anonim mesaj gönderildi");
+  document.getElementById("msg").value = "";
+}
+
+// RECEIVE (sadece alıcı açınca)
+let myId = localStorage.getItem("uid");
+if (myId) {
+  let key = "inbox_" + myId;
+  let inbox = JSON.parse(localStorage.getItem(key) || "[]");
+  if (inbox.length) {
+    let msgs = JSON.parse(localStorage.getItem("messages"));
+    msgs.push(...inbox);
+    localStorage.setItem("messages", JSON.stringify(msgs));
+    localStorage.removeItem(key);
+    loadMessages();
+  }
+}
